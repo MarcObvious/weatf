3fb8c3cf5eafc0e2a97ff11af041abd17d73f724
Marc <cat166@gmail.com>
2017-05-31 15:24:45 +0200

    kpis done

--- src/app/charts/chart.js
+++ src/app/charts/chart.js
@@ -19,14 +19,11 @@
 
     app.controller('ChartController', ['$log', '$q', '$scope', '$state', 'chartService',
         function ($log, $q, $scope, $state, chartService) {
-            /*
-             Chart.defaults.global.colours = [
-             '#97BBCD', // blue '#DCDCDC', // light grey '#F7464A', // red #46BFBD', // green  '#FDB45C', // yellow   '#949FB1', // grey '#4D5360'  // dark grey ];
-             */
+
             var init = function () {
                 $log.info('App:: Starting chartController');
-                $scope.model={};
-                $scope.model.pageTitle = $state.current.data.pageTitle;
+
+                $scope.filterName = 'KPIs';
                 var date = new Date();
 
                 $scope.dateStart = {};
@@ -42,100 +39,43 @@
                 $scope.dateEnd.date = date;
 
                 $scope.stats = {
-                    d_app: {d:'Descargas APP', v: 10000,n:''},
-                    o_app: {d:'Aperturas por push', v: 99215}, n:'',
-                    buyers: {d:'Ha comprado', v: 15550, n:''},
-                    r_compra: {d:'Recurrència de compra', v: 10000, n:''},
-                    r_visita: {d:'Recurrencia de visita', v: 10000, n:''},
-                    s_app: {d:'Han compartido la APP', v: 10000, n:''},
-                    opt_in: {d:'Optin', v: 10000, n:'%'},
-                    buscador: {d:'Han utilizado el buscador', v: 10000, n:''},
-                    p_paypal: {d:'Han pagado con paypal', v: 10000, n:''},
-                    p_targeta: {d:'Han pagado con tarjeta', v: 10000, n:''},
+                    app_downloads: {d:'Descargas APP', v: 0},
+                    count_open_push: {d:'Aperturas por push', v: 0},
+                    count_unique_buyers: {d:'Han comprado', v: 0},
+                    buy_recurrency: {d:'Recurrència de compra', v: 0},
+/*
+                    r_visita: {d:'Recurrencia de visita', v: 0},
+*/
+                    user_shares: {d:'Han compartido la APP', v: 0},
+                    count_optin_users: {d:'Optin', v: 0},
+                    count_search_users: {d:'Han utilizado el buscador', v: 0},
+                    count_success_paid: {d:'Han pagado con exito', v: 0}
                 };
 
-                $scope.filterName = 'KPIs';
+                $scope.mostrar();
+            };
 
-                $scope.succerr = {};
-                $scope.instdest = {};
+            $scope.mostrar = function () {
+                var params = {};
+                if ($scope.dateStart.date && $scope.dateEnd.date) {
+                    params.startDate =  $scope.dateStart.date.toJSON().substr(0,10);
+                    params.endDate=  $scope.dateEnd.date.toJSON().substr(0,10);
+                }
 
-                $scope.$watch('dateStart.date', function(change){
-                    reloadGraphs();
+                chartService.getKpis(params).then(function(kpis){
+                    angular.forEach(kpis,function (value, key) {
+                        if (angular.isDefined($scope.stats[key])) {
+                            $scope.stats[key].v = value;
+                        }
+                    });
+                }, function (err) {
                 });
-
-                $scope.$watch('dateEnd.date', function(change){
-                    reloadGraphs();
-                });
-
             };
 
             $scope.openDatepicker = function(date) {
                 $scope[date].opened = true;
             };
 
-            var reloadGraphs = function () {
-                var params = {
-                    start: Date.parse($scope.dateStart.date),
-                    end: Date.parse($scope.dateEnd.date)
-                };
-
-                $q.all([chartService.getChart(0, params), chartService.getChart(1, params,1), chartService.getChart(1, params,2)]).then(function(data){
-                    populateChartSuccErr(data[0],1); //IOS
-                    populateChartInstDest(data[1],1);
-                    
-                    populateChartSuccErr(data[0],2); //Android
-                    populateChartInstDest(data[2],2);
-                    
-                }, function (err) {
-                    $log.debug(err);
-                });
-            };
-
-            var populateChartSuccErr = function(chartsData, platform) {
-                delete $scope.succerr[platform];
-                $scope.succerr[platform] = {
-                    series: ['Sent', 'Succes', 'Received', 'Open', 'Failure'],
-                    colours:['#97BBCD', '#DCDCDC', '#FDB45C', '#46BFBD','#F7464A'],
-                    labels:[],
-                    data: [[], [], [], [], []],
-                    onClick: function (points, evt) {
-                        console.log(points, evt);
-                    }
-                };
-
-                angular.forEach(chartsData, function(chart){
-                    if (chart['PlatformId'] === platform) {
-                        $scope.succerr[platform].labels.push(chart['createdAt'].slice(5, 10));
-                        $scope.succerr[platform].data[0].push(chart['Sent'] ? chart['Sent'] : 0);
-                        $scope.succerr[platform].data[1].push(chart['Success'] ? chart['Success'] : 0);
-                        $scope.succerr[platform].data[2].push(chart['Received'] ? chart['Received'] : 0);
-                        $scope.succerr[platform].data[3].push(chart['Open'] ? chart['Open'] : 0);
-                        $scope.succerr[platform].data[4].push(chart['Failure'] ? chart['Failure'] : 0);
-                    }
-                });
-            };
-
-            populateChartInstDest = function(chartsData, platform) {
-                delete $scope.instdest[platform];
-                $scope.instdest[platform] = {
-                    series: ['New Successes', 'New errors'],
-                    colours:['#46BFBD', '#F7464A'],
-                    labels:[],
-                    data: [[], []],
-                    onClick: function (points, evt) {
-                        console.log(points, evt);
-                    }
-                };
-
-                angular.forEach(chartsData, function(chart){
-                    if (chart['PlatformId'] === platform) {
-                        $scope.instdest[platform].labels.push(chart['createdAt'].slice(5, 10));
-                        $scope.instdest[platform].data[0].push(chart['installed']);
-                        $scope.instdest[platform].data[1].push(chart['desinstalled']);
-                    }
-
-                });
-            };
             init();
 
         }]);
--- src/app/charts/chartService.js
+++ src/app/charts/chartService.js
@@ -9,7 +9,7 @@ angular.module('chartService', [])
                         if (!extra_route) {
                             extra_route = '';
                         }
-                        return $resource(API_URL + '/charts/' + extra_route, {}, {
+                        return $resource(API_URL + '/' + extra_route, {}, {
                             query: {
                                 timeout: 15000
                             },
@@ -27,45 +27,16 @@ angular.module('chartService', [])
                             }
                         });
                     },
-                   /* getChart: function (method, params, params_extr) {
+                    getKpis: function (params) {
                         var def = $q.defer();
-                        var extra_route = '';
-                        if (params.start && params.end){
-                            extra_route = method + '/' + params.start + '/' + params.end;
-                        }
-                        else if (params.id_campaign) {
-                            extra_route = method + '/' + params.id_campaign;
-                        }
-                        if (params_extr) {
-                            extra_route += '/' + params_extr;
-                        }
-                        this.api(extra_route).get({}, {}, function (data) {
+                        this.api('getallkpis/').save({}, params, function(data){
                             def.resolve(data.data);
                         }, function (err) {
                             def.reject(err);
                         });
                         return def.promise;
-                    },*/
-                    getChart: function (method, params, params_extr) {
-                        var def = $q.defer();
-                        var j = '';
-                        if (params_extr===1) {
-                            j = '1.json';
-                        }
-                        else {
-                            j = '2.json';
-                        }
-                        $http.get('assets/'+j).success(function(data) {
-                            def.resolve(data.data);
-                        });
-                        //var data = JSON.parse('/charts/2.json');
-                        //this.api(extra_route).get({}, {}, function (data) {
-
-                       // }, function (err) {
-                         //   def.reject(err);
-                       // });
-                        return def.promise;
                     }
+
                 };
             }]);
 
